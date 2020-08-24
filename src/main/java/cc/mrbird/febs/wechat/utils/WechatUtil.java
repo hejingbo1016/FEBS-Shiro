2e4efd12b30f09b0efeaaae23599f6bc0fa03f3e
qq781846712 <781846712@qq.com>
2020-08-24 21:25:40 +0800

    获取用户列表

--- src/main/java/cc/mrbird/febs/wechat/utils/WechatUtil.java
+++ src/main/java/cc/mrbird/febs/wechat/utils/WechatUtil.java
@@ -4,13 +4,16 @@ import cc.mrbird.febs.common.utils.ConfigUtil;
 import cc.mrbird.febs.common.utils.SpringContextUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.StrUtil;
 import org.springframework.data.redis.core.RedisTemplate;
 import weixin.popular.api.TokenAPI;
 import weixin.popular.api.UserAPI;
 import weixin.popular.bean.token.Token;
 import weixin.popular.bean.user.FollowResult;
+import weixin.popular.bean.user.User;
 import weixin.popular.bean.user.UserInfoList;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class WechatUtil {
@@ -36,10 +39,23 @@ public class WechatUtil {
         }
     }
 
-    public static void getUser() {
+    public static void getUserList(String nextOpenid, List<User> userList) {
         String token = getToken();
-        FollowResult followResult = UserAPI.userGet(token, "");
-        String[] openidArr = followResult.getData().getOpenid();
-        UserInfoList userInfoList = UserAPI.userInfoBatchget(token, "zh_CN", CollUtil.toList(openidArr));
+        FollowResult followResult = UserAPI.userGet(token, nextOpenid);
+
+        // 是否还有用户
+        String next_openid = followResult.getNext_openid();
+        if (StrUtil.isNotBlank(next_openid)) {
+            getUserList(next_openid, userList);
+        }
+
+        FollowResult.Data data = followResult.getData();
+        if (data != null) {
+            String[] openidArr = data.getOpenid();
+            if (openidArr != null && openidArr.length > 0) {
+                UserInfoList userInfoList = UserAPI.userInfoBatchget(token, "zh_CN", CollUtil.toList(ArrayUtil.sub(openidArr, 0, 10)));
+                userList.addAll(userInfoList.getUser_info_list());
+            }
+        }
     }
 }
