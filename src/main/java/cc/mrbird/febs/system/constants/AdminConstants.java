1f04ab5bb0583d0c4dd5abad637f73a7463a1fee
hejingbo <qq1134855536@qq.com>
2020-09-06 13:57:52 +0800

    调整会议管理功能

---
+++ src/main/java/cc/mrbird/febs/system/constants/AdminConstants.java
@@ -0,0 +1,33 @@
+package cc.mrbird.febs.system.constants;
+
+
+public interface AdminConstants {
+
+
+    /**
+     * 数据有效
+     */
+    Integer DATA_VALID = 1;
+
+    /**
+     * 数据无效
+     */
+    Integer DATA_INVALID = 0;
+
+    /**
+     * 数据删除
+     */
+    Integer DATA_Y_DELETED = 1;
+
+    /**
+     * 数据未删除
+     */
+    Integer DATA_N_DELETED = 0;
+
+    /**
+     * 会议审核通过
+     */
+    Integer AUDIT_T_TYPE = 1;
+
+
+}
