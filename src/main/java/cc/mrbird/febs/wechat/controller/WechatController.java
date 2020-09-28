1d306ee98326bfadea231efb4351aac37295d0e5
hejingbo <qq1134855536@qq.com>
2020-09-28 22:32:11 +0800

    会议订单明细功能开发

--- src/main/java/cc/mrbird/febs/wechat/controller/WechatController.java
+++ src/main/java/cc/mrbird/febs/wechat/controller/WechatController.java
@@ -3,26 +3,14 @@ package cc.mrbird.febs.wechat.controller;
 import cc.mrbird.febs.common.annotation.ControllerEndpoint;
 import cc.mrbird.febs.common.controller.BaseController;
 import cc.mrbird.febs.common.dto.ResponseDTO;
-import cc.mrbird.febs.system.entity.WechatUser;
-import cc.mrbird.febs.system.service.IPhoneService;
 import cc.mrbird.febs.system.service.IWechatUserService;
 import cc.mrbird.febs.wechat.dto.GetCodeDTO;
-import cc.mrbird.febs.wechat.utils.WechatUtil;
 import cc.mrbird.febs.wechat.utils.WeiChatRequestUtils;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import weixin.popular.bean.user.User;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -64,7 +52,7 @@ public class WechatController extends BaseController {
 
 
     @ControllerEndpoint(operation = "根据code，把未存入数据库中的用户存进去", exceptionMessage = "* 根据openid，把未存入数据库中的用户存进去失败")
-    @RequestMapping(value = "/getUserByOpenid")
+    @RequestMapping(value = "/getTestByCode")
     public void getTestByCode(String code) {
         wechatUserService.getTestByCode(code);
     }
